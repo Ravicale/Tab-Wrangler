e398e6df3c6b2e9f8eb92431de175e571bfc4fa6
Ravicale <rjwiedenroth@crimson.ua.edu>
2018-05-23 14:33:17 -0400

    Updated manifest, .js file, and readme. Remapped mute/unmute to Alt+M. Added mute all but current tab. Removed the switching tabs option since the default Ctrl+Page up/down command exists. Added Todo to readme and updated commands to match.

--- js/tabwrangler.js
+++ js/tabwrangler.js
@@ -24,10 +24,24 @@ function MuteUnmuteAll() {
 	browser.tabs.query({currentWindow: true}, function (tabs) {
 		for (i = 0; i < tabs.length; i++) {
 			if (tabs[i].audible) {
-//				console.log("muting", tabs[i].index);
+//				console.log("muting", tabs[i].index, " Muted- ", tabs[i].muted, " Audible- ", tabs[i].audible); //It seems like the returned promise doesn't contain the info? Possibly use .then.
 				browser.tabs.update(tabs[i].id, {muted: true});
 			} else {
-//				console.log("unmuting", tabs[i].index);
+//				console.log("unmuting", tabs[i].index, " Muted- ", tabs[i].muted, " Audible- ", tabs[i].audible);
+				browser.tabs.update(tabs[i].id, {muted: false});
+			}
+		}
+	});
+}
+
+function MuteUnmuteAlmostAll() {
+	browser.tabs.query({currentWindow: true}, function (tabs) {
+		for (i = 0; i < tabs.length; i++) {
+			if (!tabs[i].active) {
+//				console.log("muting", tabs[i].index, " Muted- ", tabs[i].muted, " Audible- ", tabs[i].audible);
+				browser.tabs.update(tabs[i].id, {muted: true});
+			} else {
+//				console.log("unmuting", tabs[i].index, " Muted- ", tabs[i].muted, " Audible- ", tabs[i].audible);
 				browser.tabs.update(tabs[i].id, {muted: false});
 			}
 		}
@@ -43,5 +57,8 @@ browser.commands.onCommand.addListener(function(command) {
 	}
 	if (command == "Mute-Unmute-All") {
 		MuteUnmuteAll();
+	}
+		if (command == "Mute-Unmute-Almost-All") {
+		MuteUnmuteAlmostAll();
 	}
 });
